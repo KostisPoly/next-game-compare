eef70ba1bbe5e34ebc652d2921e821178ccb2297
Kostis <polu_kostas@yahoo.gr>
2022-08-10 20:14:50 +0300

    Add trpc and reorder structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,36 @@
 import type { NextPage } from 'next'
+import { trpc } from '@/utils/trpc';
 
 const Home: NextPage = () => {
+  const { data, isLoading } = trpc.useQuery(["hello", { text: "Polymeros"}])
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div
+      className='h-screen w-screen flex flex-col justify-center items-center'
+    >
+      <div
+        className='text-2xl text-center pb-6'
+      >
+        Pick your game of choise!
+      </div>
+      <div
+        className='border rounded p-8 flex justify-between items-center max-w-2xl'
+      >
+        <div
+          className='w-16 h-16 bg-red-500'
+        >
+
+        </div>
+        <div
+          className='m-8'
+        >VS</div>
+        <div
+          className='w-16 h-16 bg-red-500'
+        >
+
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home
